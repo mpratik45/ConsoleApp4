a33d9ee976da1235c35f48ae47d8760bdced891e
mpratik45 <mpratik45@gmail.com>
2022-08-26 10:37:03 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQToObject
+{
+
+    public class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public string CompanyName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id}->{name}->{price}->{CompanyName}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<product> products = new List<product>()
+            {
+                new product { id = 1, name = "Mouse", price = 799, CompanyName = "Dell" },
+                new product { id = 2, name = "Mouse", price = 699, CompanyName = "Lenovo" },
+                new product { id = 3, name = "Laptop", price = 34799, CompanyName = "Dell" },
+                new product { id = 4, name = "Laptop", price = 45799, CompanyName = "Sony" },
+                new product { id = 5, name = "Laptop", price = 73899, CompanyName = "Lenovo" },
+                new product { id = 6, name = "Keyboard", price = 599, CompanyName = "Dell" },
+                new product { id = 7, name = "Keyboard", price = 999, CompanyName = "Microsoft" },
+                new product { id = 8, name = "RAM 4GB", price = 2799, CompanyName = "Intel" },
+                new product { id = 9, name = "Speaker", price = 5799, CompanyName = "Sony" },
+                new product { id = 10, name = "USB", price = 1299, CompanyName = "Dell" }
+            };
+
+            var result = from p in products
+                         select p;
+
+
+
+
+
+            var result1 = from p in products
+                          where p.price < 2000
+                          select p;
+            // OR Lambda expression
+            var res = products.Where(p => p.price < 2000).ToList();
+
+
+
+
+
+
+            var result2 = from p in products
+                          where p.price > 2000 && p.price < 3000
+                          select p;
+            //OR Lambda expression
+            var res2 = products.Where(p => p.price > 2000).OrderBy(x => x.name).ToList();
+
+
+
+
+            var result3 = from p in products
+                          where p.CompanyName.StartsWith("D")
+                          select p;
+
+            var result4 = from p in products
+                          where p.CompanyName.EndsWith("l")
+                          select p;
+            var result5 =from p in products
+                         where p.CompanyName.Contains("e")
+                         select p;
+
+            var result6 = from p in products
+                          where p.price < 2500
+                          orderby p.name descending
+                          select p;
+
+
+
+
+
+            
+            // retrive single record
+            var res4 = products.Where(x => x.id == 1).SingleOrDefault();
+
+
+
+
+
+            var res3 = products.Where(x => x.price < 2500).OrderBy(x => x.price).ToList().Take(3);
+            var res1 = products.Where(x => x.price < 2500).OrderBy(x => x.price).ToList().Skip(3);
+
+
+
+
+
+
+            foreach (product data in result6)
+            {
+                Console.WriteLine(data);
+            }
+        }   
+    }
+}
---
+++ LINQEmployee/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQEmployee
+{
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string City { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Salary} {City} ";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> emp = new List<Employee>
+            {
+                new Employee { Id=1,Name="Pratik",Salary=2700,City="Nashik"},
+                new Employee { Id=2,Name="Kunal",Salary=25000,City="Pune"},
+                new Employee { Id=3,Name="Amish",Salary=20000,City="Mumbai"},
+                new Employee { Id=4,Name="Joy",Salary=35000,City="Pune"},
+                new Employee { Id=5,Name="Pradnesh",Salary=25000,City="Pune"},
+                new Employee { Id=6,Name="Karan",Salary=43000,City="Mumbai"},
+                new Employee { Id=7,Name="Rohit",Salary=22000,City="Nashik"},
+            };
+
+            var result1 = from e in emp
+                          select e;
+            
+
+
+            var result2 = from e in emp
+                          orderby e.Name
+                          select e;
+            var res2 = emp.OrderBy(e => e.Name).ToList();//Lambda Expression
+
+
+
+
+
+            var result3 = from e in emp
+                          where e.Salary > 25000
+                          select e;
+            var res3 = emp.Where(e => e.Salary > 25000).ToList();//Lambda Expression
+
+
+
+
+
+            var result4 = from e in emp
+                          where e.City.Contains("Pune")
+                          select e;
+            var res4 = emp.Where(e=>e.City.Contains("Pune")).ToList();//Lambda Expression
+
+
+
+
+
+            var result5 = from e in emp
+                          orderby e.Salary descending 
+                          select e;
+            var res5 = emp.OrderByDescending(e => e.Salary).ToList();//Lambda Expression
+
+
+
+
+            var result6 = from e in emp
+                          where e.Name.StartsWith("P")
+                          select e;
+            var res6 = emp.Where(e => e.Name.StartsWith("P")).ToList();//Lambda Expression
+
+
+
+
+            var result7 = from e in emp
+                          where e.Salary>25000 && e.City=="Mumbai"
+                          select e;
+            var res7 = emp.Where(e => e.Salary > 25000 && e.City.Contains("Mumbai"));//Lambda Expression
+
+
+
+
+
+
+            foreach (Employee data in res7)
+            {
+                Console.WriteLine(data);
+            }
+
+        }
+    }
+}
